6c4f0cc272a9c4696632a09d1c45d291730e8b55
Z2Z ATL <105867455+Z2ZATL@users.noreply.github.com>
2024-04-04 22:45:40 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,11 +1,10 @@
 const Eris = require("eris");
 const keep_alive = require('./keep_alive.js')
 
-// Replace TOKEN with your bot account's token
 const bot = new Eris(process.env.token);
 
 bot.on("error", (err) => {
-  console.error(err); // or your preferred logger
+  console.error(err);
 });
 
-bot.connect(); // Get the bot to connect to Discord
+bot.connect();
